d0525158c2c53e8185e8f3ff71c71ec01bd70d35
root <root@mail.best-it-academy.com>
2026-02-11 14:52:12 +0000

    fix: merge conflict

---
+++ gunicorn_config.py
@@ -0,0 +1,37 @@
+"""Gunicorn configuration file for production deployment"""
+
+import multiprocessing
+
+# Server socket
+bind = "127.0.0.1:8001"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
+worker_connections = 1000
+max_requests = 1000
+max_requests_jitter = 50
+timeout = 30
+keepalive = 2
+
+# Logging
+errorlog = "logs/gunicorn-error.log"
+accesslog = "logs/gunicorn-access.log"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = "bestit_django"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL (if needed)
+# keyfile = None
+# certfile = None
